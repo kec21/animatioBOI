bded584bfa3b1ae342560f20b511f8fd577b0d7e
Kat Chudy <kec21@fsu.edu>
2022-10-21 11:29:00 -0400

    two moving rects

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 function setup() {
   createCanvas(1000, 1000);
+
 }
 
 function draw() {
   background(100);
-  describe()
-  describeElement()
+  rect(frameCount*.2,100,200,300,);
+  rect(200,frameCount,200,300,);
+
   //screenreader woo
 }
