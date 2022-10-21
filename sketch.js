fba3aefba7672332cbba381cac68354720437f5b
Kat Chudy <kec21@fsu.edu>
2022-10-21 11:40:06 -0400

    rect repeat

--- sketch.js
+++ sketch.js
@@ -1,12 +1,16 @@
-function setup() {
-  createCanvas(1000, 1000);
+let x = 0;
+let CanvasHeight = 1000;
+let CanvasWidth = 1000;
 
+function setup() {
+  createCanvas(CanvasWidth, CanvasHeight);
 }
 
 function draw() {
   background(100);
+  if (x == CanvasWidth) {
+    x = 0;
+  }
   rect(frameCount*.2,100,200,300,);
-  rect(200,frameCount,200,300,);
-
-  //screenreader woo
+  rect(200,x++,200,300,);
 }
