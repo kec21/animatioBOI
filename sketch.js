4e3c75e40ee58e4cc4a4a48f7f2dd5f8192d15ac
Kat Chudy <kec21@fsu.edu>
2022-10-26 18:48:08 -0400

    attempt 1 to set up file

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+//I am still out of town and having lots of internet issues. I get back Sunday evening. I will try to catch up as fast as I can. 
+
 let x = 0;
 let CanvasHeight = 1000;
 let CanvasWidth = 1000;
